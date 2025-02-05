b131b08a08f98281f33f45a994b1a2c49cabcf9f
MrPALpi <kapralov-02@bk.ru>
2025-02-05 10:20:27 +0500

    Updates

--- assets/error-layout-BAUGH8fj.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c}from"./index-dCgz1KnC.js";const o={};function t(n,a){return r(),c("div")}const _=e(o,[["render",t]]);export{_ as default};
---
+++ assets/error-page-DtyTJcNJ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as c}from"./index-BW6Pwd1S.js";const o={};function t(n,a){return c(),r("div")}const _=e(o,[["render",t]]);export{_ as default};
